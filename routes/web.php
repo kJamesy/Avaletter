bc26e72036852f9d6a153bb575a6ebecaa1d96f4
James Ilaki <james@acw.uk.com>
2016-11-02 15:58:10 +0000

    Capture result

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,8 @@ Route::any('webhooks', function () {
 Route::get('lab', function() {
     $email = \App\Email::getEmail(1);
 
-    \Illuminate\Support\Facades\Mail::to(\App\User::find(1))->send(new \App\Mail\Newsletter($email));
+    $result = \Illuminate\Support\Facades\Mail::to(\App\User::find(1))->send(new \App\Mail\Newsletter($email));
+    var_dump($result);
 });
 
 Route::get('/', function () {
