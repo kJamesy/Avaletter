dbcc80f428175e357f5f03754f895bb5382717d1
James Ilaki <james@acw.uk.com>
2016-11-04 13:30:18 +0000

    Attempt to use a template instead

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Email;
 use App\EmailEdition;
+use App\EmailTemplate;
 use App\Jobs\SendNewsletter;
 use App\Mail\Newsletter;
 use App\MailingList;
@@ -110,7 +111,9 @@ class EmailController extends Controller
 
             $subscribers = Subscriber::whereIn('id', [1,2,3,4])->get();
 
-            $job = (new SendNewsletter($email, $subscribers)); //->delay(Carbon::now()->addMinutes(2));
+            $template = EmailTemplate::find(17);
+
+            $job = (new SendNewsletter($template, $subscribers)); //->delay(Carbon::now()->addMinutes(2));
             dispatch($job);
 
 //            $newsletter = new Newsletter($email, $subscribers);
